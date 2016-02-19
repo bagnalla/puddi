0e4362bfefe15af0df430de8f3fef3bcf2f61fee
Alexander Bagnall <ab667712@ohio.edu>
2016-02-19 18:37:58 -0500

    Removed all the models not being used in the grumpy project along with their textures. Added some rounded object models for tokens. The rounded cube is actually pretty nice, even with textures.

--- include/SyntaxParser.h
+++ include/SyntaxParser.h
@@ -2,6 +2,7 @@
 #define PARSER_H
 
 #include "DrawableObject.h"
+#include "Schematic.h"
 #include "Token.h"
 #include "AST.h"
 #include <vector>
@@ -20,6 +21,7 @@ namespace grumpy
     {
     public:
 		SyntaxParser(Object* par, ASTNode *root);
+		SyntaxParser(Object* par, ASTNode *root, puddi::SchematicNode *schematic);
 
         void Update();
 
@@ -36,6 +38,7 @@ namespace grumpy
         SyntaxParserState state;
         glm::vec4 homePosition;
 
+		void init(ASTNode *root);
         void createNodesVector();
         void addToNodesVectorRecursive(ASTNode *node);
     };
